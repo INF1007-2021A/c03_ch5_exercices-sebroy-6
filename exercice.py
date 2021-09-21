5630045c5bc7de7f61d9b676fe8dca58bd0bec2f
sebroy-6 <sebastien-6.roy@polymtl.ca>
2021-09-21 10:19:21 -0400

    Completed Version

--- exercice.py
+++ exercice.py
@@ -4,31 +4,94 @@
 
 from typing import List
 
-
 def convert_to_absolute(number: float) -> float:
-    return 0
+    if number < 0:
+        number *= -1
+    return number
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-
-    return [""]
+    List = []
+    for letter in prefixes:
+        List.append(letter + suffixe)
+    return List
 
 
 def prime_integer_summation() -> int:
-    return 0
+    Sum = 3    # Pourquoi est-ce que ma loop considère pas 3 comme un nombre premier?
+    is_prime = True
+    number = 1
+    nb_premiers = 1
+    while nb_premiers !=100:
+        number += 1
+        for i in range(2, number):
+            if number % i == 0 :
+                is_prime = False
+                break
+        if is_prime :
+            Sum += number
+            nb_premiers += 1
+            is_prime = False
+        else :
+            is_prime = True
+
+    return Sum
 
 
 def factorial(number: int) -> int:
-    return 0
+    result = 1
+    for i in range(2, number + 1):
+        result *= i
+    return result
 
 
 def use_continue() -> None:
-    pass
+    for number in range(1,11):
+        if number == 5:
+            continue
+        print(number)
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    Acceptance_list = []
+    is_50years = False
+    isMore_70_years = False
+
+    for group in groups:
+        is_accepted = True  # reset bool values for the next group in the first dimension list
+        good_length = True
+
+        if len(group) > 10 or len(group) <= 3: # condition 1
+            is_accepted = False
+            good_length = False
+
+        if good_length:
+            for member_age in group:
+                if member_age < 18:  # condition 2
+                    is_accepted = False
+                    break
+
+                elif member_age == 50:  # condition 3
+                    is_50years = True
+                elif member_age > 70:
+                    isMore_70_years = True
+
+            if is_50years and isMore_70_years:
+                is_accepted = False
+                is_50years, isMore_70_years = False, False
+
+        if good_length:
+            for member_age in group:
+                if member_age == 25:  # condition 4
+                    is_accepted = True
+
+        if is_accepted:  # Here is where we add to the Acceptance_list the decision of accepted or not
+            Acceptance_list.append(True)
+        if is_accepted != True:
+            Acceptance_list.append(False)
+
+    return Acceptance_list
 
 
 def main() -> None:
